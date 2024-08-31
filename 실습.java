3d854ba025e19e90d97bec3a2f7e2de7c0e98cac
황순빈 <ghkdtnsqls04@gmail.com>
2024-08-31 23:27:09 +0900

    java 실습

---
+++ 실습.java
@@ -0,0 +1,41 @@
+package com.ohgiraffers.section01.literal;
+
+public class 실습 {
+    // 다음 코드의 출력 결과를 예측하세요.
+    public static void main(String[] args) {
+    System.out.println("Sum of 7 and 3: " + 7 + 3);
+    System.out.println("Sum of 7 and 3: " + (7 + 3));
+    System.out.println("Difference of 7 and 3: " + 7 - 3);
+    System.out.println("Product of 7 and 3: " + 7 * 3);
+    System.out.println("Quotient of 7 divided by 3: " + 7 / 3);
+    System.out.println("Remainder of 7 divided by 3: " + 7 % 3);
+    }
+}
+//  1."Sum of 7 and 3: 103"
+
+// 문자열 "Sum of 7 and 3: "과 숫자 7, 그리고 3이 문자열로 결합되어 "Sum of 7 and 3: 103"이 됩니다.
+
+// 2."Sum of 7 and 3: 10"
+
+// 괄호 안의 7 + 3이 먼저 계산되어 10, 문자열 "Sum of 7 and 3: "과 결합되어 "Sum of 7 and 3: 10"이 됩니다.
+
+// 3."Difference of 7 and 3: 4"
+
+// 문자열 "Difference of 7 and 3: "과 7 - 3이 계산된 결과 4, 결합되어 "Difference of 7 and 3: 4"이 됩니다.
+
+// 4."Product of 7 and 3: 21"
+
+// 문자열 "Product of 7 and 3: "과 7 * 3이 계산된 결과 21, 결합되어 "Product of 7 and 3: 21"이 됩니다.
+
+// 5."Quotient of 7 divided by 3: 2"
+
+// 문자열 "Quotient of 7 divided by 3: "과 7 / 3이 계산된 결과 2, 결합되어 "Quotient of 7 divided by 3: 2"이 됩니다.
+
+// 6."Remainder of 7 divided by 3: 1"
+
+// 문자열 "Remainder of 7 divided by 3: "과 7 % 3이 계산된 결과 1, 결합되어 "Remainder of 7 divided by 3: 1"이 됩니다.
+
+
+
+
+
